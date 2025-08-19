6529bc69672aa25c566886af9b1d96ce9378be75
Plantinhas <rayssa.olliveira31@gmail.com>
2025-08-19 12:56:07 -0300

    Initial Commit

---
+++ willamy.plantas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  willamy.plantas
+//
+//  Created by iredefbmac_35 on 19/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ willamy.plantas/willamy_plantasApp.swift
@@ -0,0 +1,17 @@
+//
+//  willamy_plantasApp.swift
+//  willamy.plantas
+//
+//  Created by iredefbmac_35 on 19/08/25.
+//
+
+import SwiftUI
+
+@main
+struct willamy_plantasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
